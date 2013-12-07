0895001331c819dc8935c208d3061cbd9ad765c2
Minecrell <dev@minecrell.net>
2013-12-07 11:30:58 +0100

    Add a command to the plugin

--- src/main/java/net/minecrell/ringbearer/RingBearer.java
+++ src/main/java/net/minecrell/ringbearer/RingBearer.java
@@ -1,6 +1,8 @@
 package net.minecrell.ringbearer;
 
 import com.google.common.base.Preconditions;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RingBearer extends JavaPlugin {
@@ -25,5 +27,11 @@ public final class RingBearer extends JavaPlugin {
     @Override
     public void onDisable() {
         this.getLogger().info(this.getName() + " disabled.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // TODO: Command handling
+        return true;
     }
 }
